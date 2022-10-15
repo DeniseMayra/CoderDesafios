96c0294c6acc2591d730321da1daff922ef481cd
Denise Mayra Higa <deniiise.mayra@gmail.com>
2022-10-15 12:37:36 -0300

    desafio corregido

--- d1-clases-HigaDenise.js
+++ d1-clases-HigaDenise.js
@@ -1,46 +1,63 @@
-class usuario{
-    constructor(name, surname){
-        this.nombre = name  
-        this.apellido = surname  
-        this.libros = [] 
-        this.mascotas = [] 
+class contenedor{
+    constructor(){
+        this.productosArray = [] 
     }
 
-    getFullName(){
-        return `${this.nombre} ${this.apellido}`
+    save(object){
+        this.productosArray.push(object)
     }
-    addMascota(name){
-        this.mascotas.push(name)
-    }
-    countMascotas(){
-        return this.mascotas.length
-    }
-    addBook(name, author){
-        const newBook = {
-            nombre: name,
-            autor: author
+    getById(idNumber){
+        for (let i = 0; i < this.productosArray.length ; i++){
+            if (this.productosArray[i].id == idNumber){
+                return this.productosArray[i]
+            }
         }
-        this.libros.push(newBook)
+        return null
     }
-    getBookNames(){
-        let bookNames = []
-        for (let i=0; i < this.libros.length; i++){
-            bookNames.push(this.libros[i].nombre)
-        }
-        return bookNames
+    getAll(){
+        return this.productosArray
+    }
+    deleteById(number){
+        const newArray = this.productosArray.filter(prod => prod.id != number)
+        this.productosArray = newArray
+    }
+    deleteAll(){
+        this.productosArray = []
     }
 }
 
-const userOne = new usuario('Denise', 'Higa')
 
-userOne.addMascota('kitty')
-userOne.addMascota('kesha')
-userOne.addBook('el hombre en busca del sentido', 'viktor frankl')
-userOne.addBook('palabras cruzadas', 'gabriel rolon')
+const prod1 = new contenedor()
 
-console.log(userOne.getFullName())
-console.log(userOne.countMascotas())
-console.log(userOne.getBookNames())
+prod1.save({
+    id: 1,
+    title: "Iphone 12 mini",
+    price: 1100,
+    thumbnail: "https://www.google.com.ar/url?sa=i&url=https%3A%2F%2Fwww.macstation.com.ar%2Fiphone-12-mini-128-gb-verde-green.html&psig=AOvVaw3nKes8YS7bIThCA-XHtBHc&ust=1665932693602000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCJDEuIXB4voCFQAAAAAdAAAAABAH"
+})
+prod1.save({
+    id: 2,
+    title: "MacBook Air",
+    price: 1500,
+    thumbnail: "https://www.google.https://www.google.com.ar/url?sa=i&url=https%3A%2F%2Fipoint.com.ar%2Fmac%2F10133-macbook-air-13-m1-2020-8-core-cpu-256-gb-space-gray-194252056172.html&psig=AOvVaw2yf0Xcvi3-u7qz_RPAtSsy&ust=1665932795304000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCIit87XB4voCFQAAAAAdAAAAABAF.ar/url?sa=i&url=https%3A%2F%2Fwww.macstation.com.ar%2Fiphone-12-mini-128-gb-verde-green.html&psig=AOvVaw3nKes8YS7bIThCA-XHtBHc&ust=1665932693602000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCJDEuIXB4voCFQAAAAAdAAAAABAH"
+})
+prod1.save({
+    id: 3,
+    title: "Airpods ",
+    price: 300,
+    thumbnail: "https://image.png.google.https://www.google.com.ar/url?sa=i&url=https%3A%2F%2Fipoint.com.ar%2Fmac%2F10133-macbook-air-13-m1-2020-8-core-cpu-256-gb-space-gray-194252056172.html&psig=AOvVaw2yf0Xcvi3-u7qz_RPAtSsy&ust=1665932795304000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCIit87XB4voCFQAAAAAdAAAAABAF.ar/url?sa=i&url=https%3A%2F%2Fwww.macstation.com.ar%2Fiphone-12-mini-128-gb-verde-green.html&psig=AOvVaw3nKes8YS7bIThCA-XHtBHc&ust=1665932693602000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCJDEuIXB4voCFQAAAAAdAAAAABAH"
+})
+console.log("metodo getAll: ")
+console.log(prod1.getAll())
 
+console.log("\nmetodo getById con id 2: ")
+console.log(prod1.getById(2))
 
+prod1.deleteById(2)
+console.log("\nmetodo eliminar el prod con id 2: ")
+console.log(prod1.getAll())
+
+prod1.deleteAll()
+console.log("\nmetodo deleteAll: ")
+console.log(prod1.getAll())
 
