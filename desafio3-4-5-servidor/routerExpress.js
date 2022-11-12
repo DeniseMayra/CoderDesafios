932633c6ca9a0cc8cc84bbebd04c15a8663f59e9
Denise Mayra Higa <deniiise.mayra@gmail.com>
2022-11-12 16:11:47 -0300

    Desafio 5-handlebars

--- desafio3-4-5-servidor/routerExpress.js
+++ desafio3-4-5-servidor/routerExpress.js
@@ -1,18 +1,22 @@
 const {Router} = require('express')
 const {container} =require('../Desafio2-archivos')
 
-const products = new container("productsD3.txt")
+const productsClass = new container("productsD3.txt")
 
 const routerProducts = new Router()
 
 routerProducts.get('/', async(req,res) => {
     //devuelve todos los productos
-    const product = await products.getAll()
-    res.json(product)
+    const products = await productsClass.getAll()
+    let haveProd = false
+    if(products.lenght != 0){
+        haveProd = true
+    }
+    res.render('list', {haveProd, products})
 })
 routerProducts.get('/:id', async(req,res) => {
     //devuelve prod segun id
-    const prodId = await products.getById(req.params.id)
+    const prodId = await productsClass.getById(req.params.id)
     if (prodId != null){
         res.json(prodId)
     } else {
@@ -23,19 +27,18 @@ routerProducts.get('/:id', async(req,res) => {
 routerProducts.post('/', async(req,res) => {
     // recibe y agrega un prod, devuelve el id
     const newProd = {...req.body}
-    const prodId = await products.save(newProd) 
+    const prodId = await productsClass.save(newProd) 
     res.status(201)
-    res.json(prodId)
+    res.redirect('/')
 })
 routerProducts.put('/:id', (req,res) => {
     //recibe y actualiza un prod segun id
-    console.log(req.body)
     const prodUpdate = {...req.body, id: req.params.id}
-    products.updateById(prodUpdate)
+    productsClass.updateById(prodUpdate)
     res.json('producto actualizado')
 })
 routerProducts.delete('/:id', (req,res) => {
-    products.deleteById(req.params.id)
+    productsClass.deleteById(req.params.id)
     res.json('eliminado correctamente')
 })
 
---
+++ desafio3-4-5-servidor/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const routerProducts = require('./routerExpress')
+const {engine} = require('express-handlebars')
+
+const app = express()
+
+// ------------- HANDLEBARS SETTING -------------
+const hbsConfig = {
+    extname: 'hbs',
+    defaultLayout: 'index.hbs'
+}
+app.engine('hbs', engine(hbsConfig))
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+// ------------- ROUTES ------------
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// app.use('/', express.static('public'))
+// app.get('/style.css', function(req,res){
+//     res.sendFile('../style.css')
+// })
+app.get('/', (req, res) => {
+    res.render('form')
+})
+app.use('/api/productos', routerProducts)
+
+// ------------- SERVER -------------
+function conectServer(port = 0){
+    return new Promise((resolve, reject) => {
+        const serverConected = app.listen(port, () => {
+            resolve(serverConected)
+        })
+        serverConected.on("error", error => reject(error))
+    })
+}
+
+module.exports = {conectServer}
